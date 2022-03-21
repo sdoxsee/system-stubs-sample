b10d8e26ef994cf47bf338cb3756745f40a9042a
Stephen Doxsee <sdoxsee@gmail.com>
2022-03-21 14:30:22 -0400

    Initial Generation

---
+++ src/main/java/com/example/systemstubssample/SystemStubsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemstubssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemStubsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemStubsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/systemstubssample/SystemStubsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemstubssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemStubsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
