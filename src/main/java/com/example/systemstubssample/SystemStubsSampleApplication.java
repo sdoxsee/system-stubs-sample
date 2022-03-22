91c8ba563a6c8af4ef5432a555365bcc7a0edbdd
Stephen Doxsee <sdoxsee@gmail.com>
2022-03-21 20:50:40 -0400

    Add tests for environment variable cases

--- src/main/java/com/example/systemstubssample/SystemStubsSampleApplication.java
+++ src/main/java/com/example/systemstubssample/SystemStubsSampleApplication.java
@@ -1,7 +1,10 @@
 package com.example.systemstubssample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SystemStubsSampleApplication {
@@ -10,4 +13,19 @@ public class SystemStubsSampleApplication {
 		SpringApplication.run(SystemStubsSampleApplication.class, args);
 	}
 
+}
+
+@RestController
+class SampleController {
+
+	private final String originalValue;
+
+	public SampleController(@Value("${SOME_KEY}") String originalValue) {
+		this.originalValue = originalValue;
+	}
+
+	@GetMapping("/")
+	public String result() {
+		return "original: " + this.originalValue + ", live: " + System.getenv("SOME_KEY");
+	}
 }
--- src/test/java/com/example/systemstubssample/SystemStubsSampleApplicationTests.java
+++ src/test/java/com/example/systemstubssample/SystemStubsSampleApplicationTests.java
@@ -1,13 +1,63 @@
 package com.example.systemstubssample;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith({SystemStubsExtension.class})
 class SystemStubsSampleApplicationTests {
 
-	@Test
-	void contextLoads() {
+	// sets the environment before Spring even starts
+	@SystemStub
+	private static EnvironmentVariables environmentVariables =
+			new EnvironmentVariables("SOME_KEY", "original value");
+
+	@Nested
+	@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+	@AutoConfigureWebTestClient
+	class SpringBootTestTest implements CommonEnvironmentTest {
 	}
 
+	@Nested
+	@WebMvcTest(SampleController.class)
+	@AutoConfigureWebTestClient
+	class WebMvcTestTest implements CommonEnvironmentTest {
+	}
+
+	interface CommonEnvironmentTest {
+
+		@Test
+		default void environmentTest(@Autowired WebTestClient webTestClient, EnvironmentVariables environmentVariables) {
+			environmentVariables.set("SOME_KEY", "new value");
+			assertThat(System.getenv("SOME_KEY")).isEqualTo("new value");
+
+			webTestClient
+					.get()
+					.uri("/")
+					.exchange()
+					.expectBody(String.class)
+					.value(Matchers.containsString("original: original value, live: new value"));
+
+			environmentVariables.set("SOME_KEY", "newer value");
+			assertThat(System.getenv("SOME_KEY")).isEqualTo("newer value");
+
+			webTestClient
+					.get()
+					.uri("/")
+					.exchange()
+					.expectBody(String.class)
+					.value(Matchers.containsString("original: original value, live: newer value"));
+		}
+	}
 }
